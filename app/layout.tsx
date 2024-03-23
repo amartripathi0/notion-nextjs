13810f0a21fc549f26582299acfbd9439e058cca
amartripathi0 <theamartripathi@gmail.com>
2024-03-23 15:25:02 +0530

    add:hero section

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Notion",
+  description: "App for making and sharing notes.",
+  icons : {
+    icon : [
+      {
+      media : "(prefers-color-scheme : light)",
+      url : "/logo.svg",
+      href : "/logo.svg"
+    },
+      {
+      media : "(prefers-color-scheme : dark)",
+      url : "/logo-dark.svg",
+      href : "/logo-dark.svg"
+    },
+    
+  ]
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
